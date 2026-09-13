b826336965d52f03d549d5adcd449a82a0f73e5b
dadoslav <radisavljevicests@gmail.com>
2026-09-13 23:45:21 +0200

    Zamenio logo u footeru u svg mada isk leva kolona moze skroz da se ukloni...

--- public/components/footer.php
+++ public/components/footer.php
@@ -5,7 +5,47 @@
     <!-- Leva kolona -->
     <div class="footer-brand">
 
-        <img src="assets/img/logo.png" alt="Dirline">
+        <!--<img src="assets/img/logo.png" alt="Dirline">-->
+        <svg width="193" height="243" viewBox="0 0 193 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.5 126.5C2.5 129.814 5.18629 132.5 8.5 132.5C11.8137 132.5 14.5 129.814 14.5 126.5C14.5 123.186 11.8137 120.5 8.5 120.5C5.18629 120.5 2.5 123.186 2.5 126.5Z" stroke="black" stroke-width="5"/>
+            <path d="M22.5 104.5C22.5 107.814 25.1863 110.5 28.5 110.5C31.8137 110.5 34.5 107.814 34.5 104.5C34.5 101.186 31.8137 98.5 28.5 98.5C25.1863 98.5 22.5 101.186 22.5 104.5Z" stroke="black" stroke-width="5"/>
+            <path d="M57.5 29.5C57.5 32.8137 60.1863 35.5 63.5 35.5C66.8137 35.5 69.5 32.8137 69.5 29.5C69.5 26.1863 66.8137 23.5 63.5 23.5C60.1863 23.5 57.5 26.1863 57.5 29.5Z" stroke="#9A42E8" stroke-width="5"/>
+            <path d="M78.5 8.5C78.5 11.8137 81.1863 14.5 84.5 14.5C87.8137 14.5 90.5 11.8137 90.5 8.5C90.5 5.18629 87.8137 2.5 84.5 2.5C81.1863 2.5 78.5 5.18629 78.5 8.5Z" stroke="#9A42E8" stroke-width="5"/>
+            <path d="M130.5 104.5H136.5C150.859 104.5 162.5 92.8594 162.5 78.5C162.5 64.1406 150.859 52.5 136.5 52.5H133.324" stroke="#5B42E8" stroke-width="5"/>
+            <path d="M110.5 126.5H138C164.786 126.5 186.5 104.786 186.5 78C186.5 51.2142 164.786 29.5 138 29.5H117.206" stroke="#5B42E8" stroke-width="5"/>
+            <path d="M130.5 104.5L34.5 104.5" stroke="url(#paint0_linear_80_102)" stroke-width="5"/>
+            <path d="M116.5 126.5L14.5 126.5" stroke="url(#paint1_linear_80_102)" stroke-width="5"/>
+            <path d="M104.5 29.5C104.5 32.8137 107.186 35.5 110.5 35.5C113.814 35.5 116.5 32.8137 116.5 29.5C116.5 26.1863 113.814 23.5 110.5 23.5C107.186 23.5 104.5 26.1863 104.5 29.5Z" stroke="#5B42E8" stroke-width="5"/>
+            <path d="M122.5 52.5C122.5 55.8137 125.186 58.5 128.5 58.5C131.814 58.5 134.5 55.8137 134.5 52.5C134.5 49.1863 131.814 46.5 128.5 46.5C125.186 46.5 122.5 49.1863 122.5 52.5Z" stroke="#5B42E8" stroke-width="5"/>
+            <path d="M57.5 217.5C57.5 220.814 60.1863 223.5 63.5 223.5C66.8137 223.5 69.5 220.814 69.5 217.5C69.5 214.186 66.8137 211.5 63.5 211.5C60.1863 211.5 57.5 214.186 57.5 217.5Z" stroke="black" stroke-width="5"/>
+            <path d="M109.5 144.5C109.5 147.814 112.186 150.5 115.5 150.5C118.814 150.5 121.5 147.814 121.5 144.5C121.5 141.186 118.814 138.5 115.5 138.5C112.186 138.5 109.5 141.186 109.5 144.5Z" stroke="black" stroke-width="5"/>
+            <path d="M116.5 182.5L148.5 223.5" stroke="black" stroke-width="5"/>
+            <path d="M119.5 150.5L181.5 229.5" stroke="black" stroke-width="5"/>
+            <path d="M106.5 176.5C106.5 179.814 109.186 182.5 112.5 182.5C115.814 182.5 118.5 179.814 118.5 176.5C118.5 173.186 115.814 170.5 112.5 170.5C109.186 170.5 106.5 173.186 106.5 176.5Z" stroke="black" stroke-width="5"/>
+            <path d="M146.5 229.5C146.5 232.814 149.186 235.5 152.5 235.5C155.814 235.5 158.5 232.814 158.5 229.5C158.5 226.186 155.814 223.5 152.5 223.5C149.186 223.5 146.5 226.186 146.5 229.5Z" stroke="black" stroke-width="5"/>
+            <path d="M178.5 234.5C178.5 237.814 181.186 240.5 184.5 240.5C187.814 240.5 190.5 237.814 190.5 234.5C190.5 231.186 187.814 228.5 184.5 228.5C181.186 228.5 178.5 231.186 178.5 234.5Z" stroke="black" stroke-width="5"/>
+            <path d="M79.5 234.5C79.5 237.814 82.1863 240.5 85.5 240.5C88.8137 240.5 91.5 237.814 91.5 234.5C91.5 231.186 88.8137 228.5 85.5 228.5C82.1863 228.5 79.5 231.186 79.5 234.5Z" stroke="black" stroke-width="5"/>
+            <path d="M63 36V211.5" stroke="url(#paint2_linear_80_102)" stroke-width="5"/>
+            <path d="M84.5 14.5L85.5266 228.5" stroke="url(#paint3_linear_80_102)" stroke-width="5"/>
+            <defs>
+            <linearGradient id="paint0_linear_80_102" x1="34.5" y1="105" x2="130.5" y2="105" gradientUnits="userSpaceOnUse">
+            <stop/>
+            <stop offset="1" stop-color="#5B42E8"/>
+            </linearGradient>
+            <linearGradient id="paint1_linear_80_102" x1="14.5" y1="127" x2="116.5" y2="127" gradientUnits="userSpaceOnUse">
+            <stop/>
+            <stop offset="1" stop-color="#5B42E8"/>
+            </linearGradient>
+            <linearGradient id="paint2_linear_80_102" x1="63.5" y1="36" x2="63.5" y2="211.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#9A42E8"/>
+            <stop offset="0.9999"/>
+            </linearGradient>
+            <linearGradient id="paint3_linear_80_102" x1="85" y1="14.4976" x2="86.0266" y2="228.497" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#9A42E8"/>
+            <stop offset="0.9999"/>
+            </linearGradient>
+            </defs>
+        </svg>
 
         <h2>DIRLINE</h2>
 
